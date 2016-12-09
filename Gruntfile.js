2afbf2fa761bb219ca1a23338798835c5c29fc16
Michael Strasser <info@m-strasser.net>
2016-12-09 17:21:58 +0100

    Added config file ability, enabled single markdown post, broke json posts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,68 +1,71 @@
 module.exports = function(grunt) {
+  var config = grunt.file.readJSON('config.json');
+  grunt.initConfig({
+      watch: {
+        pug: {
+          files: ['pug/*.pug'],
+          tasks: ['pug']
+        },
+        sass: {
+          files: ['sass/*.scss'],
+          tasks: ['sass']
+        }
+      },
+      pug: {
+        options: {
+          pretty: true,
+        data: {
+          articles: function(articles) {
+            if(config.articles.json) {
+              return require('./pug/articles/articles.json');
+            }else if(config.articles.markdown) {
+              return grunt.file.read('./pug/articles/article.md');
+            }
+          },
+          require: require
+        }
+        },
+        compile: {
+          files: [{
+            cwd: 'pug/',
+            src: '**/*.pug',
+            dest: 'public/',
+            expand: true,
+            ext: '.html'
+          }]
+        }
+      },
+      sass: {
+        dist: {
+          files: [{
+            cwd: 'sass/',
+            src: '**/*.scss',
+            dest: 'public/css/',
+            expand: true,
+            ext: '.css'
+          }]
+        }
+      },
+      shell: {
+        options: {
+          stderr: false
+        },
+        target: {
+          command: [
+            'if [ ! -d "signatures" ]; then mkdir signatures; fi',
+            'if [ ! -d "signatures/css" ]; then mkdir signatures/css; fi',
+            'gpg2 --batch --yes --armor --detach-sig public/*.html',
+            'mv public/*.html.asc signatures/',
+                  'gpg2 --batch --yes --armor --detach-sig public/css/*.css',
+                  'mv public/css/*.css.asc signatures/css/',
+          ].join('&&')
+        },
+      }
+    });
 
-        grunt.initConfig({
-                watch: {
-                        pug: {
-                                files: ['pug/*.pug'],
-                                tasks: ['pug']
-                        },
-                        sass: {
-                                files: ['sass/*.scss'],
-                                tasks: ['sass']
-                        }
-                },
-                pug: {
-                      options: {
-                        pretty: true,
-                        data: function(articles) {
-                          return require('./pug/articles/articles.json');
-                        }
-                      },
-                        compile: {
-                                files: [{
-                                        cwd: 'pug/',
-                                        src: '**/*.pug',
-                                        dest: 'public/',
-                                        expand: true,
-                                        ext: '.html'
-                                }]
-                        }
-                },
-                sass: {
-                        dist: {
-                                files: [{
-                                        cwd: 'sass/',
-                                        src: '**/*.scss',
-                                        dest: 'public/css/',
-                                        expand: true,
-                                        ext: '.css'
-                                }]
-                        }
-                },
-                shell: {
-                        options: {
-                                stderr: false
-                        },
-                        target: {
-                                command: [
-                                        'if [ ! -d "signatures" ]; then mkdir signatures; fi',
-                                        'if [ ! -d "signatures/css" ]; then mkdir signatures/css; fi',
-                                        'gpg2 --batch --yes --armor --detach-sig public/*.html',
-                                        'mv public/*.html.asc signatures/',
-                                        'gpg2 --batch --yes --armor --detach-sig public/css/*.css',
-                                        'mv public/css/*.css.asc signatures/css/',
-                                ].join('&&')
-                        },
-                }
-        });
-        // grunt.event.on('watch', function(action, filepath, target) {
-        //         grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
-        // });
-
-        grunt.loadNpmTasks('grunt-contrib-watch');
-        grunt.loadNpmTasks('grunt-contrib-pug');
-        grunt.loadNpmTasks('grunt-sass');
-        grunt.loadNpmTasks('grunt-shell');
-        grunt.registerTask('build', ['pug', 'sass', 'shell']);
-
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-pug');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.registerTask('build', ['pug', 'sass', 'shell']);
 };
---
+++ pug/articles/article.md
@@ -0,0 +1,7 @@
+# Hello, World!
+
+I am a beautiful *markdown* article, it's just amazing to write this sort of stuff in markdown :) My god, I can't believe it!
+
+```js
+console.log("Do I look like javascript?");
+```
