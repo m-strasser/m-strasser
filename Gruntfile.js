83b71b652693ecef62ccba339b2b15e9a4495880
Michael Strasser <info@m-strasser.net>
2016-12-09 11:14:07 +0100

    Prettify compiled HTML

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,9 @@ module.exports = function(grunt) {
                         }
                 },
                 pug: {
+                        options: {
+                                pretty: true
+                        },
                         compile: {
                                 files: [{
                                         cwd: 'pug/',
